86e99a05365c47282ef3c5d82d3b298a0953c56c
nk-the-crazy <nurkali@gmail.com>
2021-04-21 12:01:20 +0600

    Add API routes for latest Coin defi-markets data

--- src/routes/markets.routes.js
+++ src/routes/markets.routes.js
@@ -7,6 +7,7 @@ const router = express.Router()
 router.get('/global/latest', (req, res) => controller.getLatestGlobalMarkets(req, res))
 router.get('/global/:period', (req, res) => controller.getGlobalMarkets(req, res))
 router.get('/defi', (req, res) => controller.getDefiMarkets(req, res))
+router.get('/defi/:coinGeckoId/latest', (req, res) => controller.getLatestCoinDefiMarkets(req, res))
 router.get('/defi/:coinGeckoId/:period', (req, res) => controller.getCoinDefiMarkets(req, res))
 
 export default router
